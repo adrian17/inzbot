f3d3a97636a33ce9512b8dedf984cc6c2fd71067
Adrian Wielgosik <adrian.wielgosik@gmail.com>
2016-02-27 20:28:29 +0100

    add chuck norris

---
+++ plugins/chuck.py
@@ -0,0 +1,18 @@
+from plugin_base import *
+
+import requests
+import html
+
+class ChuckPlugin(Plugin):
+
+    @command("chuck", "norris")
+    def chuck(self, bot, event):
+        """!chuck => Fetches a Chuck Norris joke."""
+        try:
+            url = "http://api.icndb.com/jokes/random"
+            json = requests.get(url).json()
+            joke = json["value"]["joke"]
+            joke = html.unescape(joke)
+            bot.message(joke)
+        except:
+            logging.exception("")
