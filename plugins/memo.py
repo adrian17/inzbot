ff2051be6ae933485d723e43143f373460f60230
Adrian Wielgosik <adrian.wielgosik@gmail.com>
2017-08-16 14:31:43 +0200

    Memos have state now

--- plugins/memo.py
+++ plugins/memo.py
@@ -2,12 +2,23 @@ from plugin_base import *
 
 from collections import defaultdict
 import arrow
+import os
+import yaml
 
 class MemoPlugin(Plugin):
+
+    path = "memos.yaml"
+
     def __init__(self):
-        import logging
         super().__init__()
-        self.memos = defaultdict(list)
+        self.memos = {}
+        if os.path.isfile(self.path):
+            with open(self.path) as datafile:
+                self.memos = yaml.load(datafile)
+
+    def save(self):
+        with open(self.path, "w") as datafile:
+            yaml.dump(self.memos, datafile, default_flow_style=False, allow_unicode=True)
 
     @command
     def tell(self, bot, event):
@@ -18,7 +29,10 @@ class MemoPlugin(Plugin):
             return
         target, message = text.split(maxsplit=1)
         target, message = target.strip().rstrip(".,:"), message.strip()[:400]
-        self.memos[target].append((event.source.nick, arrow.now(bot.timezone), message))
+        self.memos.setdefault(target, []).append(
+            (event.source.nick, arrow.now(bot.timezone), message)
+        )
+        self.save()
         bot.message("Consider it noted.")
 
     @on_pubmsg
@@ -29,3 +43,4 @@ class MemoPlugin(Plugin):
                 time = time.format("HH:mm")
                 bot.message("{}: {} | {} | {}".format(nick, source, time, message))
             del self.memos[nick]
+            self.save()
